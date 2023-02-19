ccf78b7cb9d2bb7e0af07151456b084df9694c8b
Luis <perezluis167@gmail.com>
2023-02-19 16:33:39 -0500

    added login

--- backend/routes/api/users.js
+++ backend/routes/api/users.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const bcrypt = require('bcryptjs');
 const mongoose = require('mongoose');
 const User = mongoose.model('User');
+const passport = require('passport');
 
 /* GET users listing. */
 router.get('/', function (req, res, next) {
@@ -47,11 +48,24 @@ router.post('/register', async (req, res, next) => {
         const user = await newUser.save();
         return res.json({ user });
       }
-      catch(err) {
+      catch (err) {
         next(err);
       }
     })
   });
+})
+
+router.post('/api/users/login', async (req, res, next) => {
+  passport.authenticate('local', async function (err, user) {
+    if (err) return next(err);
+    if (!user) {
+      const err = new Error('Invalid credentials');
+      err.statusCode = 400;
+      err.errors = { email: "Invalid credentials" };
+      return next(err);
+    }
+    return res.json({ user });
+  })(req, res, next);
 })
 
 module.exports = router;
